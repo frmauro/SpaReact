a6bbf4843e457e6b798007c537365b1c3b93eecf
francisco <frmauro8@hotmail.com>
2020-02-21 06:52:20 -0300

    My first commit

---
+++ src/components/Contato/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+
+
+function Contato() {
+  return (
+    <div className="home" id="contato">
+      <h1>Entre em contato</h1>
+      <h2>Telefone: (XX) 9 9999-9999</h2>
+      <h3>Rua: Alguma rua, endereço</h3>
+      <Link smooth to="#home" style={{color: '#fff'}}>Ir para Home</Link><br></br>
+      <Link smooth to="#sobre" style={{color: '#fff'}}>Ir para Sobre</Link>
+    </div>
+  );
+}
+
+export default Contato;
---
+++ src/components/Inicio/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Inicio() {
+  return (
+    <div className="home">
+      <h1>Bem-vindo a  Home</h1>
+      <h2>Aprendendo spa com React JS na pratica</h2>
+      <h3>Evoluindo a cada dia!</h3>
+    </div>
+  );
+}
+
+export default Inicio;
